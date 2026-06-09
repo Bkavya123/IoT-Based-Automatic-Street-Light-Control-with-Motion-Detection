c8469c2931eaa252f9ee7cb7799b54e6148ac84b
Bonala kavya <bonalakavya90@gmail.com>
2026-06-09 18:47:06 +0530

    code for arduino

---
+++ code.ino
@@ -0,0 +1,35 @@
+int pirPin = 2;
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+int led4 = 11;
+void setup()
+{
+ pinMode(pirPin, INPUT);
+ pinMode(led1, OUTPUT);
+ pinMode(led2, OUTPUT);
+ pinMode(led3, OUTPUT);
+ pinMode(led4, OUTPUT);
+ Serial.begin(9600);
+}
+void loop()
+{
+ int motion = digitalRead(pirPin);
+ if (motion == HIGH)
+ {
+ digitalWrite(led1, HIGH);
+ digitalWrite(led2, HIGH);
+ digitalWrite(led3, HIGH);
+ digitalWrite(led4, HIGH);
+ Serial.println("Motion Detected");
+ }
+ else
+ {
+ digitalWrite(led1, LOW);
+ digitalWrite(led2, LOW);
+ digitalWrite(led3, LOW);
+ digitalWrite(led4, LOW);
+ Serial.println("No Motion");
+ }
+ delay(100);
+}
